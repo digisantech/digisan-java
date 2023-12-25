96b41809d428e3c4dcfa521d261bba894d60bc11
Lucas Moreira <eskerlucas1@gmail.com>
2023-12-25 18:26:44 -0300

    Update API URL

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 <dependency>
     <groupId>br.com.digisan</groupId>
     <artifactId>digisan-java</artifactId>
-    <version>1.0.9</version>
+    <version>1.0.10</version>
 </dependency>
 ```
--- src/main/java/br/com/digisan/model/Digisan.java
+++ src/main/java/br/com/digisan/model/Digisan.java
@@ -2,7 +2,7 @@ package br.com.digisan.model;
 
 public abstract class Digisan {
 
-    public static final String ENDPOINT = "https://api.digisan.com.br/diginfe";
+    public static final String ENDPOINT = "https://api.digisan.com.br";
     public static final String VERSION = "1.0.0";
     private static String apiKey;
     public static void init(String apiKey) {
